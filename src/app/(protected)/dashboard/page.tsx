39dfcee53b0fc379838b32f782fc087ea89fb9fd
Ankit Singh <ankitjasbeersingh@gmail.com>
2025-01-08 14:16:59 +0530

    added Github commit loader and commit hash table

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import useProject from "@/hooks/use-project";
-import { useUser } from "@clerk/nextjs";
 import { ExternalLink, Github } from "lucide-react";
 import Link from "next/link";
 
@@ -25,7 +24,21 @@ const DashboardPage = () => {
           </div>
 
         </div>
+        <div className="h-4"></div>
+        <div className="flex items-center gap-4">
+          TeamMembers
+          InviteButton
+          ArchiveButton
+        </div>
       </div>
+      <div className="mt-4">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-5">
+          AskQuestionCard
+          MeetingCard
+        </div>
+      </div>
+      <div className="mt-8"></div>
+      CommitLog
     </div>
   )
 }
